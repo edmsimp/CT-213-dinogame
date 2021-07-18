386b1a054e859dd6e2beac506049cdfe3b3c78e3
edmsimp <63891357+edmsimp@users.noreply.github.com>
2021-07-18 19:30:54 -0300

    dqn training com graficos + salvamento de pesos

--- dqnagent.py
+++ dqnagent.py
@@ -11,7 +11,7 @@ class Agent:
     """
     Represents a Deep Q-Networks (DQN) agent.
     """
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size, action_size, gamma=0.95, epsilon=0.5, epsilon_min=1e-7, epsilon_decay=0.9, learning_rate=2e-5, buffer_size=4098):
         """
         Creates a Deep Q-Networks (DQN) agent.
 
@@ -35,9 +35,15 @@ class Agent:
         self.state_size = state_size
         self.height, self.width, self.channels = self.state_size    
         self.action_size = action_size
-        self.model = self.make_model(self.height, self.width, self.channels, self.action_size)
+        self.replay_buffer = deque(maxlen=buffer_size)  # giving a maximum length makes this buffer forget old memories
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.learning_rate = learning_rate
+        self.model = self.make_model()
 
-    def make_model(self, height, width, channels, actions):
+    def make_model(self):
         """
         Makes the action-value neural network model using Keras.
 
@@ -47,24 +53,16 @@ class Agent:
         # Todo: Uncomment the lines below CHECK
         model = models.Sequential()
         # Todo: implement Keras' model CHECK      
-        model.add(layers.Convolution2D(32, (8,8), strides=(4,4), activation=activations.relu, input_shape=(3, height, width, channels), padding="same"))
+        model.add(layers.Convolution2D(32, (8,8), strides=(4,4), activation=activations.relu, input_shape=(self.height, self.width, self.channels), padding="same"))
         model.add(layers.Convolution2D(64, (4,4), strides=(2,2), activation=activations.relu, padding="same"))
         model.add(layers.Convolution2D(64, (3,3), strides=(1,1), activation=activations.relu, padding="same"))
         model.add(layers.Flatten())
         model.add(layers.Dense(512, activation='relu'))
         model.add(layers.Dense(256, activation='relu'))
-        model.add(layers.Dense(actions, activation='linear'))        
+        model.add(layers.Dense(self.action_size, activation='linear'))
+        model.compile(loss=losses.mse, optimizer=optimizers.Adam(lr=self.learning_rate))        
         model.summary()
         return model
-
-    def build_agent(self, model, actions):
-        policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.2, nb_steps=10000)
-        memory = SequentialMemory(limit=1000, window_length=3)
-        dqn = DQNAgent(model=model, memory=memory, policy=policy,
-                  enable_dueling_network=True, dueling_type='avg', 
-                   nb_actions=actions, nb_steps_warmup=1000
-                  )
-        return dqn
 
     def act(self, state):
         """
@@ -86,3 +84,81 @@ class Agent:
             num_actions = action_values.shape[1]
             random_action = np.random.randint(num_actions)
             return random_action
+
+    def append_experience(self, state, action, reward, next_state, done):
+        """
+        Appends a new experience to the replay buffer (and forget an old one if the buffer is full).
+
+        :param state: state.
+        :type state: NumPy array with dimension (1, 2).
+        :param action: action.
+        :type action: int.
+        :param reward: reward.
+        :type reward: float.
+        :param next_state: next state.
+        :type next_state: NumPy array with dimension (1, 2).
+        :param done: if the simulation is over after this experience.
+        :type done: bool.
+        """
+        self.replay_buffer.append((state, action, reward, next_state, done))
+
+    def replay(self, batch_size):
+        """
+        Learns from memorized experience.
+
+        :param batch_size: size of the minibatch taken from the replay buffer.
+        :type batch_size: int.
+        :return: loss computed during the neural network training.
+        :rtype: float.
+        """
+        minibatch = random.sample(self.replay_buffer, batch_size)
+        states, targets = [], []
+        for state, action, reward, next_state, done in minibatch:
+            target = self.model.predict(state)
+            if not done:
+                target[0][action] = reward + self.gamma * np.max(self.model.predict(next_state)[0])
+            else:
+                target[0][action] = reward
+            # Filtering out states and targets for training
+            states.append(state[0])
+            targets.append(target[0])
+        history = self.model.fit(np.array(states), np.array(targets), epochs=1, verbose=0)
+        # Keeping track of loss
+        loss = history.history['loss'][0]
+        return loss
+
+    def load(self, name):
+        """
+        Loads the neural network's weights from disk.
+
+        :param name: model's name.
+        :type name: str.
+        """
+        self.model.load_weights(name)
+
+    def save(self, name):
+        """
+        Saves the neural network's weights to disk.
+
+        :param name: model's name.
+        :type name: str.
+        """
+        self.model.save_weights(name)
+
+    def update_epsilon(self):
+        """
+        Updates the epsilon used for epsilon-greedy action selection.
+        """
+        self.epsilon *= self.epsilon_decay
+        if self.epsilon < self.epsilon_min:
+            self.epsilon = self.epsilon_min
+
+    #FUNÇÃO A SER RETIRADA CASO SEJA USADA A ESTRUTURA DO MANGA
+    def build_agent(self, model, actions):
+        policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.2, nb_steps=10000)
+        memory = SequentialMemory(limit=1000, window_length=3)
+        dqn = DQNAgent(model=model, memory=memory, policy=policy,
+                  enable_dueling_network=True, dueling_type='avg', 
+                   nb_actions=actions, nb_steps_warmup=1000
+                  )
+        return dqn
